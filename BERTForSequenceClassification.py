26161b23c2045d59fcaf6c74350749c5a94370d6
genderbias <73666643+genderbias@users.noreply.github.com>
2020-10-30 05:11:18 +0330

    Update BERTForSequenceClassification.py

--- BERTForSequenceClassification.py
+++ BERTForSequenceClassification.py
@@ -16,6 +16,7 @@ class_names = ['Female', 'Male' , 'Neutral']
 PRE_TRAINED_MODEL_NAME = 'bert-base-uncased'
 MAX_LEN = 33
 TRAIN_BATCH_SIZE = 16
+VALID_BATCH_SIZE = 16
 EPOCHS = 8
 LEARNING_RATE = 2e-5
 tokenizer = BertTokenizer.from_pretrained(PRE_TRAINED_MODEL_NAME)
